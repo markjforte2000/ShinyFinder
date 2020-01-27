4cff5cae06272ccb3878cbf6d26318577770fbf4
MarkForte <mjforte@wpi.edu>
2020-01-27 15:42:00 -0500

    Added logger file support

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -3,40 +3,78 @@ from datetime import datetime
 
 class Logger(object):
 
-    def __init__(self):
+    __info_flag = "INFO"
+    __debug_flag = "DEBUG"
+    __warning_flag = "WARNING"
+    __error_flag = "ERROR"
+
+    def __init__(self, log_file=None, automatic_file_writes=True):
         # default format
         # "[TIME] [TYPE] [SOURCE]: MESSAGE"
-        self.default_format_source = "[{0}] [{1}] [{2}]: {3}"
-        self.default_format_no_source = "[{0}] [{1}]: {2}"
-        self.info_flag = "INFO"
-        self.debug_flag = "DEBUG"
-        self.warning_flag = "WARNING"
-        self.error_flag = "ERROR"
+        # "[TIME] [TYPE]: MESSAGE"
+        self.__default_format_source = "[{0}] [{1}] [{2}]: {3}"
+        self.__default_format_no_source = "[{0}] [{1}]: {2}"
+        # setup stream saving
+        self.__sorted_stream = {
+            self.__info_flag: [],
+            self.__debug_flag: [],
+            self.__warning_flag: [],
+            self.__error_flag: []
+        }
+        self.__stream = []
+        # log file
+        self.__log_file = log_file
+        self.__automatic_file_writes = automatic_file_writes
         return
 
-    def _log(self, log_type, data, source=None):
+    def __log(self, log_type, data, source=None):
         now = datetime.now()
         now = now.strftime("%m-%d-%Y %H:%M:%S")
         if source is None:
-            message = self.default_format_no_source.format(now, log_type, data)
+            message = self.__default_format_no_source.format(now, log_type, data)
         else:
-            message = self.default_format_source.format(now, log_type, source, data)
+            message = self.__default_format_source.format(now, log_type, source, data)
+
+        self.__sorted_stream[log_type].append(message)
+        self.__stream.append(message)
         print(message)
+        if self.__log_file is not None and self.__automatic_file_writes:
+            message = message + "\n"
+            self.__log_file.write(message)
         return
+
+    def write_to_file(self):
+        if self.__log_file is not None:
+            for message in self.__stream:
+                self.__log_file.write(message)
 
     def info(self, data, source=None):
-        self._log(self.info_flag, data, source)
+        self.__log(self.__info_flag, data, source)
         return
+
+    def get_info(self):
+        return self.__sorted_stream[self.__info_flag]
 
     def debug(self, data, source=None):
-        self._log(self.debug_flag, data, source)
+        self.__log(self.__debug_flag, data, source)
         return
+
+    def get_debug(self):
+        return self.__sorted_stream[self.__debug_flag]
 
     def warning(self, data, source=None):
-        self._log(self.warning_flag, data, source)
+        self.__log(self.__warning_flag, data, source)
         return
+
+    def get_warning(self):
+        return self.__sorted_stream[self.__warning_flag]
 
     def error(self, data, source=None):
-        self._log(self.error_flag, data, source)
+        self.__log(self.__error_flag, data, source)
         return
 
+    def get_error(self):
+        return self.__sorted_stream[self.__error_flag]
+
+    def get_stream(self):
+        return self.__stream
--- tests/logger_test.py
+++ tests/logger_test.py
@@ -1,16 +1,38 @@
 from src import logger
 
+from os import remove
 
-logger = logger.Logger()
+
+# default config test
+print("-- DEFAULT OUTPUTS TEST --")
+lgr = logger.Logger()
 
 print("Test with source")
-logger.info("info", source="TEST")
-logger.debug("debug", source="TEST")
-logger.warning("warning", source="TEST")
-logger.error("error", source="TEST")
+lgr.info("info", source="TEST")
+lgr.debug("debug", source="TEST")
+lgr.warning("warning", source="TEST")
+lgr.error("error", source="TEST")
 print("Test without source")
-logger.info("info")
-logger.debug("debug")
-logger.warning("warning")
-logger.error("error")
+lgr.info("info")
+lgr.debug("debug")
+lgr.warning("warning")
+lgr.error("error")
+
+# output with file
+log_file = open("log_test_file.txt", "w")
+lgr2 = logger.Logger(log_file=log_file)
+
+print("-- MULTIPLE OUTPUTS TEST --")
+
+lgr2.info("info", source="TEST2")
+lgr2.debug("debug", source="TEST2")
+lgr2.warning("warning", source="TEST2")
+lgr2.error("error", source="TEST2")
+
+log_file.close()
+
+print("Check log_test_file.txt")
+print("Then press enter")
+input()
+remove("log_test_file.txt")
 
