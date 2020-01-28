71e3d06e1cf46d1c35032054b883a032c7065ec5
Mark Forte <markjforte2000@gmail.com>
2020-01-28 15:56:50 -0500

    minor fixes

--- src/controller/assistant/battle_controller.py
+++ src/controller/assistant/battle_controller.py
@@ -6,7 +6,7 @@ current_angle = 0
 
 def walk_in_circle(controller):
     global current_angle
-    current_angle += 45
+    current_angle += 70
     current_angle = current_angle % 360
     controller.set_joystick_angle(angle=current_angle, joystick=Reference.JOYSTICK)
     return
--- src/controller/assistant/menu_controller.py
+++ src/controller/assistant/menu_controller.py
@@ -6,7 +6,8 @@ def run(controller):
     Run from wild battle
     Hits joystick up then a button
     """
-    controller.set_joystick_angle(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH)
+    controller.set_joystick_angle(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH)  # move to run option
     controller.release_joystick(joystick=Reference.JOYSTICK, delay=Reference.BUTTON_PRESS_TIME)
-    controller.press_button(button=Reference.A, delay=.5)
+    controller.press_button(button=Reference.A, delay=Reference.BUTTON_PRESS_TIME * 2)  # select run
+    controller.press_button(button=Reference.A, delay=Reference.BUTTON_PRESS_TIME * 4)  # just to be safe
 
