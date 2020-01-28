1eb10bc9c55fb2866e9176a8b3b5c8fd60af45c0
Mark Forte <markjforte2000@gmail.com>
2020-01-27 21:35:33 -0500

    joystick debug

--- src/controller/assistant/menu_controller.py
+++ src/controller/assistant/menu_controller.py
@@ -7,6 +7,6 @@ def run(controller):
     Hits joystick up then a button
     """
     controller.set_joystick_angle(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH)
-    controller.release_joystick(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH, delay=0.1)
+    controller.release_joystick(joystick=Reference.JOYSTICK, delay=0.1)
     controller.press_button(button=Reference.A, delay=.5)
 
