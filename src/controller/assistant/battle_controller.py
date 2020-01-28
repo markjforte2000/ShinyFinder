d65cb41b0d1bee9c6a5c3251e8df75833575d7b6
Mark Forte <markjforte2000@gmail.com>
2020-01-27 20:45:36 -0500

    joystick debug

--- src/controller/assistant/battle_controller.py
+++ src/controller/assistant/battle_controller.py
@@ -7,6 +7,6 @@ current_angle = 0
 def walk_in_circle(controller):
     global current_angle
     current_angle += 10
-
+    current_angle = current_angle % 360
     controller.set_joystick_angle(angle=current_angle, joystick=Reference.JOYSTICK)
     return
