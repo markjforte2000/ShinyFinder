4a56009b76e4d97b5c37da50e5199737c854f4ca
Mark Forte <markjforte2000@gmail.com>
2020-01-27 20:41:50 -0500

    joystick debug

--- src/controller/assistant/battle_controller.py
+++ src/controller/assistant/battle_controller.py
@@ -6,7 +6,7 @@ current_angle = 0
 
 def walk_in_circle(controller):
     global current_angle
-    current_angle += 1
+    current_angle += 10
 
     controller.set_joystick_angle(angle=current_angle, joystick=Reference.JOYSTICK)
     return
--- src/controller/assistant/menu_controller.py
+++ src/controller/assistant/menu_controller.py
@@ -7,8 +7,6 @@ def run(controller):
     Hits joystick up then a button
     """
     controller.set_joystick_angle(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH)
-    controller.set_joystick_angle(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH,
-                                  delay=Reference.BUTTON_PRESS_TIME)
-    controller.press_button(button=Reference.A, delay=2 * Reference.BUTTON_PRESS_TIME)
-    controller.press_button(button=Reference.A, delay=6 * Reference.BUTTON_PRESS_TIME)
+    controller.release_joystick(joystick=Reference.JOYSTICK, angle=Reference.JOYSTICK_NORTH, delay=0.1)
+    controller.press_button(button=Reference.A, delay=.5)
 
